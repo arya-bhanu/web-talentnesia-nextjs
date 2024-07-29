4d1b316ffcea3cfd02dfdcd98d9908ba17379c63
arya-bhanu <aryacreative.dev@gmail.com>
2024-07-29 20:43:25 +0700

    feat: responsive footer

--- src/portal/components/Footer.tsx
+++ src/portal/components/Footer.tsx
@@ -65,19 +65,19 @@ const dataNavs = [
 ];
 const Footer = ({ className }: { className?: string }) => {
 	return (
-		<footer className={clsx(className, 'py-12')}>
-			<div className='container flex justify-between'>
+		<footer className={clsx(className, 'py-4 md:py-9 lg:py-12')}>
+			<div className='container flex justify-between md:flex-row flex-col gap-3 md:gap-0'>
 				<div>
 					<Link href={'/'}>
 						<LogoCompany />
 					</Link>
-					<SocialMedia className='mt-7' />
-					<p className='mt-4 font-inter text-[#999999] text-sm'>
+					<SocialMedia className='mt-3 sm:mt-5 lg:mt-7' />
+					<p className='mt-3 sm:mt-4 font-inter text-[#999999] text-sm'>
 						Copyright 2023 Talentnesia - All Right Reserved
 					</p>
 				</div>
-				<div className='w-1/2 '>
-					<div className='flex items-start gap-x-20'>
+				<div className=' w-full md:w-1/2 '>
+					<div className='flex items-start gap-4 sm:gap-5 md:gap-x-12 lg:gap-x-20 flex-wrap'>
 						{dataNavs.map((dataNav, index) => (
 							<NavLinks
 								key={index}
@@ -85,14 +85,14 @@ const Footer = ({ className }: { className?: string }) => {
 							/>
 						))}
 					</div>
-					<div className='flex items-center justify-between mt-12 pt-6 border-t border-t-[#E7E9EE]'>
+					<div className='flex flex-wrap items-center justify-between mt-3 md:mt-10 lg:mt-12 pt-2 sm:pt-4 md:pt-6 border-t border-t-[#E7E9EE] gap-3'>
 						<div>
 							<h4 className='text-[#00B3AD] font-semibold'>GABUNG KOMUNITAS</h4>
-							<p className='text-[#2D2D2D] text-sm font-semibold mt-2'>
+							<p className='text-[#2D2D2D] text-sm font-semibold mt-1 md:mt-2'>
 								Jadi Bagian dari Komunitas Animasi Terbesar di Indonesia
 							</p>
 						</div>
-						<button className='flex items-center gap-2 rounded-full px-6 py-2.5 border border-[#E7E9EE]'>
+						<button className='flex items-center gap-2 md:text-base text-sm rounded-full  px-3 md:px-6 py-1 md:py-2.5 border border-[#E7E9EE]'>
 							<span>Visit Here</span>
 							<Arrow />
 						</button>
--- src/portal/components/NavLinks.tsx
+++ src/portal/components/NavLinks.tsx
@@ -9,7 +9,7 @@ const NavLinks = ({ title, links }: { title: string; links: ILink[] }) => {
 	return (
 		<div className='font-inter'>
 			<h4 className='font-semibold text-[#2D2D2D]'>{title}</h4>
-			<nav className='flex flex-col gap-1.5 mt-3'>
+			<nav className='flex flex-col gap-0.5 md:gap-1.5 mt-1 md:mt-3'>
 				{links.map((link, index) => (
 					<Link
 						href={link.link}
