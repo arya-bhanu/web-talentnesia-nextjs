2b568bb28f7b2999162f6fab424a123eedb4f95c
MohZainulAdib <m.zainul.adib.developer@gmail.com>
2024-07-31 12:05:51 +0700

    refactor

--- src/portal/modules/home/components/user-story/UserStory.view.tsx
+++ src/portal/modules/home/components/user-story/UserStory.view.tsx
@@ -2,11 +2,11 @@ import UserStoryCard from '@/portal/components/UserStoryCard';
 import React from 'react';
 
 const UserStorySectionView = ({ className }: { className?: string }) => {
-	return (
-		<section className={className}>
-			<UserStoryCard />
-		</section>
-	);
+  return (
+    <section className={className}>
+      <UserStoryCard />
+    </section>
+  );
 };
 
 export default UserStorySectionView;
