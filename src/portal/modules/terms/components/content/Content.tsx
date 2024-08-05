80d64bfa8f9ad04a419cdf6fb07e0a9883e41eec
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-08-05 21:42:20 +0700

    fix responsive content

--- src/portal/modules/terms/components/content/Content.tsx
+++ src/portal/modules/terms/components/content/Content.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ContentView } from './Content.view';
-import { contentData } from './content.data';
 import clsx from 'clsx';
 
 export const Content = ({ className }: { className?: string }) => {
