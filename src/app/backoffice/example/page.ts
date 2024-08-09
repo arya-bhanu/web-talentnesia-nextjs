f6f42bfb34508d4b044bd4eb4ad8a2d4a6824539
arya-bhanu <aryacreative.dev@gmail.com>
2024-08-09 07:06:48 +0700

    integrate API

--- src/app/backoffice/example/page.ts
+++
@@ -1,3 +0,0 @@
-import Example from '@/backoffice/modules/example/Example';
-
-export default Example;
--- src/backoffice/modules/example/Example.tsx
+++ src/backoffice/modules/example/Example.tsx
@@ -1,15 +1,21 @@
+'use client';
 import React from 'react';
 import ExampleView from './Example.view';
 import FormExample from './components/form-example/FormExample';
 import TableExample from './components/table-example/TableExample';
+import { useQuery } from '@tanstack/react-query';
+import { backOfficeAPI } from '@/lib/axiosConfig';
 
-const Example: React.FC = () => {
-  return (
-    <ExampleView>
-      <FormExample />
-      <TableExample />
-    </ExampleView>
-  );
+const Example = () => {
+  const { data } = useQuery({
+    queryKey: ['example'],
+    queryFn: async () => {
+      const data = await backOfficeAPI.get('/example');
+      return data;
+    },
+  });
+  console.log(data?.data);
+  return <h1>Hello World</h1>;
 };
 
 export default Example;
