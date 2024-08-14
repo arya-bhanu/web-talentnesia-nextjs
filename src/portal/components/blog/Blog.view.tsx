1775474880ed8b3a0c5fc14b798350fdd8481cea
frnzsiscus <150585154+frnnzsiscus@users.noreply.github.com>
2024-08-14 10:54:31 +0700

    blog navbar

--- src/portal/components/blog/Blog.view.tsx
+++ src/portal/components/blog/Blog.view.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Breadcrumb } from '@/portal/components/breadcrumb';
 import { IoEyeOutline } from 'react-icons/io5';
 import { Article } from './blog.type';
-import { FaArrowRightLong } from "react-icons/fa6";
+import { FaArrowRightLong } from 'react-icons/fa6';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface BlogviewProps {
   articles: Article[];
@@ -28,44 +30,46 @@ const Blogview: React.FC<BlogviewProps> = ({ articles, currentPage, totalPages, 
               </p>
             </div>
 
-          <div className='mt-9'>
-          <p className="max-w-2xl text-xl text-gray-700 pt-5 font-inter pb-4">
-            Kategori
-          </p>
-          <div className="flex flex-wrap items-center gap-3 md:gap-4">
-            <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-8 py-3 mx-2">
-              All
-            </button>
-            <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-6 py-3 mx-2">
-              Animation
-            </button>
-            <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-6 py-3 mx-2">
-              Digital Marketing
-            </button>
-            <div className="relative ml-auto mt-4 md:mt-0 w-full max-w-lg">
-              <input 
-                type="text" 
-                name="Search" 
-                className="w-full border h-12 shadow p-4 rounded-full text-gray-800 border-gray-300 bg-white" 
-                placeholder="Search" 
-              />
-              <button type="submit" className="absolute top-3.5 right-3">
-                <svg className="text-gray-400 h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
-                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>
-                </svg>
-              </button>
+            <div className='mt-9'>
+              <p className="max-w-2xl text-xl text-gray-700 pt-5 font-inter pb-4">
+                Kategori
+              </p>
+              <div className="flex flex-wrap items-center gap-3 md:gap-4">
+                <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-8 py-3 mx-2">
+                  All
+                </button>
+                <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-6 py-3 mx-2">
+                  Animation
+                </button>
+                <button type="button" className="text-black font-inter bg-[#FFC862] hover:bg-blue-100 border border-[#D0D5DD] focus:outline-none focus:ring-4 rounded-full text-sm px-6 py-3 mx-2">
+                  Digital Marketing
+                </button>
+                <div className="relative ml-auto mt-4 md:mt-0 w-full max-w-lg">
+                  <input 
+                    type="text" 
+                    name="Search" 
+                    className="w-full border h-12 shadow p-4 rounded-full text-gray-800 border-gray-300 bg-white" 
+                    placeholder="Search" 
+                  />
+                  <button type="submit" className="absolute top-3.5 right-3">
+                    <svg className="text-gray-400 h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.966 56.966">
+                      <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
 
             <div className="mt-12">
               <h3 className="text-2xl font-poppins font-bold text-[#344054] mb-6">Terpopuler</h3>
               <div className="mx-auto grid gap-5 lg:max-w-none lg:grid-cols-2">
+                <Link href="/blog/singleblog">
                 <article className="relative flex flex-col rounded-2xl overflow-hidden w-full group cursor-pointer">
-                  <img
+                  <Image
                     src="/img/blog/writing.jpg"
-                    alt="Article Image"
+                    width={500}
+                    height={250}
+                    alt="Using Notion To Make Portfolio"
                     className="w-full h-[250px] object-cover transition duration-300 ease-in-out group-hover:scale-110"
                   />
                   <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-6">
@@ -79,11 +83,15 @@ const Blogview: React.FC<BlogviewProps> = ({ articles, currentPage, totalPages, 
                     </div>
                   </div>
                 </article>
-
+                </Link>
+                
+                <Link href="/blog/singleblog">
                 <article className="relative flex flex-col rounded-2xl overflow-hidden w-full group cursor-pointer">
-                  <img
+                  <Image
                     src="/img/blog/teacher.jpg"
-                    alt="Article Image"
+                    width={500}
+                    height={250}
+                    alt="Teacher Image"
                     className="w-full h-[250px] object-cover transition duration-300 ease-in-out group-hover:scale-110"
                   />
                   <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-end p-6">
@@ -97,6 +105,7 @@ const Blogview: React.FC<BlogviewProps> = ({ articles, currentPage, totalPages, 
                     </div>
                   </div>
                 </article>
+                </Link>
               </div>
             </div>
 
@@ -130,8 +139,10 @@ const Blogview: React.FC<BlogviewProps> = ({ articles, currentPage, totalPages, 
               <div className="mx-auto grid gap-5 lg:max-w-none lg:grid-cols-4">
                 {articles.map((article, index) => (
                   <article key={index} className="relative flex flex-col rounded-2xl overflow-hidden w-full group cursor-pointer">
-                    <img
+                    <Image
                       src={article.imageSrc}
+                      width={500}
+                      height={250}
                       alt={article.title}
                       className="w-full h-[250px] object-cover transition duration-300 bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-100 ease-in-out group-hover:brightness-50"
                     />
