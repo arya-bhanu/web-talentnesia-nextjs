848bb30db9d5a14a8955c84f526165c0704864ea
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-08-16 18:06:47 +0700

    add table religion, role, quiz-type

---
+++ src/app/backoffice/master-data/quiz-type/page.tsx
@@ -0,0 +1,9 @@
+
+import QuizType from '@/backoffice/modules/master-data/quiz-type';
+import React from 'react';
+
+const AcademicTitlePage = () => {
+  return <QuizType />;
+};
+
+export default AcademicTitlePage;
