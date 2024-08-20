c23f1cfdaae7fb2b236b57894ea2b12fdadb335d
shincy1 <tangocraft0@gmail.com>
2024-08-20 12:47:57 +0700

    new file:   src/backoffice/modules/user/admin/Admin.tsx 	new file:   src/backoffice/modules/user/admin/Admin.view.tsx 	new file:   src/backoffice/modules/user/admin/__test__/admin.test.tsx 	new file:   src/backoffice/modules/user/admin/admin.data.ts 	new file:   src/backoffice/modules/user/admin/admin.style.css 	new file:   src/backoffice/modules/user/admin/admin.type.ts 	new file:   src/backoffice/modules/user/admin/api/admins.ts 	new file:   src/backoffice/modules/user/admin/hooks/admins.ts 	new file:   src/backoffice/modules/user/admin/index.ts 	new file:   src/backoffice/modules/user/mentor/Mentor.tsx 	new file:   src/backoffice/modules/user/mentor/Mentor.view.tsx 	new file:   src/backoffice/modules/user/mentor/__test__/Mentors.test.tsx 	new file:   src/backoffice/modules/user/mentor/api/mentors.ts 	new file:   src/backoffice/modules/user/mentor/hooks/mentors.ts 	new file:   src/backoffice/modules/user/mentor/index.ts 	new file:   src/backoffice/modules/user/mentor/mentor.data.ts 	new file:   src/backoffice/modules/user/mentor/mentor.style.css 	new file:   src/backoffice/modules/user/mentor/mentor.type.ts 	new file:   src/backoffice/modules/user/school-operator/SchoolOperator.tsx 	new file:   src/backoffice/modules/user/school-operator/SchoolOperator.view.tsx 	new file:   src/backoffice/modules/user/school-operator/__test__/schoolOperators.test.tsx 	new file:   src/backoffice/modules/user/school-operator/api/schoolOperators.ts 	new file:   src/backoffice/modules/user/school-operator/hooks/schoolOperators.ts 	new file:   src/backoffice/modules/user/school-operator/index.ts 	new file:   src/backoffice/modules/user/school-operator/schoolOperator.data.ts 	new file:   src/backoffice/modules/user/school-operator/schoolOperator.style.css 	new file:   src/backoffice/modules/user/school-operator/schoolOperator.type.ts 	new file:   src/backoffice/modules/user/student/Student.tsx 	new file:   src/backoffice/modules/user/student/Student.view.tsx 	new file:   src/backoffice/modules/user/student/__test__/students.test.tsx 	new file:   src/backoffice/modules/user/student/api/students.ts 	new file:   src/backoffice/modules/user/student/hooks/students.ts 	new file:   src/backoffice/modules/user/student/index.ts 	new file:   src/backoffice/modules/user/student/student.data.ts 	new file:   src/backoffice/modules/user/student/student.style.css 	new file:   src/backoffice/modules/user/student/student.type.ts

---
+++ src/backoffice/modules/user/school-operator/index.ts
@@ -0,0 +1,1 @@
+export { SchoolOperator } from './SchoolOperator';
---
+++ src/backoffice/modules/user/student/student.data.ts
@@ -0,0 +1,69 @@
+// Static dropdown options data
+export const religions = [
+  'Islam',
+  'Kristen',
+  'Katholik',
+  'Hindu',
+  'Buddha',
+  'Lainnya',
+];
+
+export const educationLevels = [
+  'SD (Sekolah Dasar)',
+  'SMP (Sekolah Menengah Pertama)',
+  'SMA (Sekolah Menengah Atas)',
+  'D1',
+  'D2',
+  'D3',
+  'D4',
+  'S1',
+  'S2',
+  'S3',
+];
+
+export const provinces = ['Jawa Timur', 'jawa Barat', 'Jawa Tengah'];
+
+export const districts = [
+  'Malang',
+  'Cirebon',
+  'Semarang',
+  'Solo',
+  'Surabaya',
+  'Jakarta',
+  'Bandung',
+  'Medan',
+  'Makassar',
+  'Palembang',
+  'Yogyakarta',
+  'Bali',
+  'Lampung',
+  'Banten',
+  'Jambi',
+  'Bali',
+  'Lampung',
+  'Banten',
+];
+
+export const subDistrict = [
+  'KedungKandang',
+  'Depok',
+  'Gunungpati',
+  'Sleman',
+  'Denpasar',
+  'Kediri',
+];
+
+export const placesOfBirth = [
+  'Jakarta',
+  'Surabaya',
+  'Bandung',
+  'Medan',
+  'Semarang',
+  'Makassar',
+  'Palembang',
+  'Yogyakarta',
+  'Bali',
+  'Lampung',
+  'Banten',
+  'Jambi',
+];
