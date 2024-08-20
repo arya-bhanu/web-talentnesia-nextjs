76e201ae631ee24cfc4f8dfad16ed8069f0f16c2
Dika <annkrey6@gmail.com>
2024-08-20 13:46:02 +0700

    feat: school admin page

---
+++ src/app/backoffice/school/detail-school/page.tsx
@@ -0,0 +1,12 @@
+import DetailSchool from '@/backoffice/modules/school/detail-course';
+import React from 'react';
+
+const AddSchoolProgram = () => {
+  return (
+    <>
+      <DetailSchool />
+    </>
+  );
+};
+
+export default AddSchoolProgram;
