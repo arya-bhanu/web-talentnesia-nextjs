0d9f1a391aa76b6e1973f70281da44208522b7ae
arya-bhanu <aryacreative.dev@gmail.com>
2024-08-23 08:16:10 +0700

    feat: fix dependencies and build error

--- src/backoffice/components/badge/Badge.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import BadgeView from './Badge.view';
-import { IBadgeType } from './badge.type';
-
-const Badge: React.FC<IBadgeType> = (props) => {
-  return <BadgeView {...props} />;
-};
-
-export default Badge;
--- src/backoffice/components/badge/badge.type.ts
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface IBadgeType {
-  color: string;
-  children: ReactNode;
-}
