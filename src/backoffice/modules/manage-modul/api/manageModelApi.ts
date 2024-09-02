67cf30a54fa8377229506e37d6705a9a8f1132b2
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-09-02 23:10:03 +0700

    fix manage module API

--- src/backoffice/modules/manage-modul/api/manageModelApi.ts
+++ src/backoffice/modules/manage-modul/api/manageModelApi.ts
@@ -1,4 +1,3 @@
-import { backOfficeAPI } from '@/lib/axiosConfig';
 import {
   APIContentChapter,
   APIExamChapter,
@@ -22,7 +21,7 @@ export const fetchModules = async () => {
     url: '/v1/manage-module',
     method: 'GET',
   });
-
+  console.log('response: fetchModules', response);
   return response.data.items;
 };
 
@@ -32,6 +31,7 @@ export const fetchModule = async (id?: string | null) => {
       url: '/v1/manage-module/' + id,
       method: 'GET',
     });
+    console.log('response: fetchModule', response.data);
     return response.data;
   }
   return null;
@@ -42,17 +42,19 @@ export const deleteModule = async (id: string) => {
     url: '/v1/manage-module/' + id,
     method: 'DELETE',
   });
+  console.log('response: deleteModule', response);
   return response;
 };
 
 export const createModul = async (
-  data: Pick<APIResponseManageModul, 'active' | 'name'>
+  data: Pick<APIResponseManageModul, 'active' | 'name'>,
 ) => {
   const response = await fetchAxios<{ data: APIResponseManageModul }>({
     url: '/v1/manage-module',
     method: 'POST',
     formData: data,
   });
+  console.log('response: createModul', response);
   return response.data;
 };
 
@@ -68,6 +70,7 @@ export const updateModul = async ({
     method: 'PUT',
     formData: data,
   });
+  console.log('response: updateModul', response);
   return response.data;
 };
 
@@ -78,7 +81,7 @@ export const fetchChapter = async (chapterId?: string | null) => {
       url: '/v1/chapter/' + chapterId,
       method: 'GET',
     });
-    console.log('response', response);
+    console.log('response: fetchChapter', response);
     return response;
   }
   return null;
@@ -91,8 +94,13 @@ export const createChapter = async ({
   moduleId: string;
   title: string;
 }) => {
-  const response = await backOfficeAPI.post('/v1/chapter', { moduleId, title });
-  return response.data;
+  const response = await fetchAxios({
+    url: '/v1/chapter',
+    method: 'POST',
+    formData: { moduleId, title },
+  });
+  console.log('response: createChapter', response);
+  return response;
 };
 
 export const editChapter = async ({
@@ -102,31 +110,49 @@ export const editChapter = async ({
   chapterId: string;
   title: string;
 }) => {
-  const response = await backOfficeAPI.put('/v1/chapter/' + chapterId, {
-    title,
+  const response = await fetchAxios({
+    url: '/v1/chapter/' + chapterId,
+    method: 'PUT',
+    formData: { title },
   });
-  return response.data;
+  console.log('response: editChapter', response);
+  return response;
 };
 
 export const deleteChapter = async (id: string) => {
-  const response = await backOfficeAPI.delete('/v1/chapter/' + id);
-  return response.data;
+  const response = await fetchAxios({
+    url: '/v1/chapter/' + id,
+    method: 'DELETE',
+  });
+  console.log('response: deleteChapter', response);
+  return response;
 };
 
 // content
 export const fetchContent = async (id?: string) => {
   if (id) {
-    const response = await backOfficeAPI.get('/v1/content/' + id);
-    return response.data;
+    const response = await fetchAxios({
+      url: '/v1/content/' + id,
+      method: 'GET',
+    });
+    console.log('response: fetchContent', response);
+    return response;
   }
   return null;
 };
+
 export const createContent = async (
   payload: Omit<APIContentChapter, 'id' | 'order'>,
 ) => {
-  const response = await backOfficeAPI.post('/v1/content', payload);
-  return response.data;
+  const response = await fetchAxios({
+    url: '/v1/content',
+    method: 'POST',
+    formData: payload,
+  });
+  console.log('response: createContent', response);
+  return response;
 };
+
 export const editContent = async ({
   id,
   data,
@@ -135,14 +161,24 @@ export const editContent = async ({
   data: Pick<APIContentChapter, 'title' | 'body' | 'type' | 'duration'>;
 }) => {
   if (id) {
-    const response = await backOfficeAPI.put('/v1/content/' + id, data);
-    return response.data;
+    const response = await fetchAxios({
+      url: '/v1/content/' + id,
+      method: 'PUT',
+      formData: data,
+    });
+    console.log('response: editContent', response);
+    return response;
   }
   return null;
 };
+
 export const deleteContent = async (id: string) => {
-  const response = await backOfficeAPI.delete('/v1/content/' + id);
-  return response.data;
+  const response = await fetchAxios({
+    url: '/v1/content/' + id,
+    method: 'DELETE',
+  });
+  console.log('response: deleteContent', response);
+  return response;
 };
 
 // exam
@@ -203,6 +239,7 @@ export const examReorder = async ({
   });
   return response.data;
 };
+
 export const contentsReorder = async ({
   contents,
   chapterId,
@@ -210,14 +247,15 @@ export const contentsReorder = async ({
   contents: string[];
   chapterId: string;
 }) => {
-  const response = await backOfficeAPI.post(
-    '/v1/content/reorder-contents/' + chapterId,
-    {
-      contents,
-    },
-  );
-  return response.data;
+  const response = await fetchAxios({
+    method: 'POST',
+    url: '/v1/content/reorder-contents/' + chapterId,
+    formData: { contents },
+  });
+  console.log('response: contentsReorder', response);
+  return response;
 };
+
 export const chapterReorder = async ({
   chapters,
   modulId,
@@ -225,11 +263,11 @@ export const chapterReorder = async ({
   modulId: string;
   chapters: string[];
 }) => {
-  const response = await backOfficeAPI.post(
-    '/v1/chapter/reorder-chapters/' + modulId,
-    {
-      chapters,
-    },
-  );
-  return response.data;
+  const response = await fetchAxios({
+    method: 'POST',
+    url: '/v1/chapter/reorder-chapters/' + modulId,
+    formData: { chapters },
+  });
+  console.log('response: chapterReorder', response);
+  return response;
 };
