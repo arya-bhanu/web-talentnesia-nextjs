77a64312be7fc7f62714acfa42bb6faf4428877a
MohZainulAdib <m.zainul.adib.developer@gmail.com>
2024-09-03 15:07:50 +0700

    fixed

--- src/backoffice/modules/master-data/academic-level/academicLevel.type.ts
+++ src/backoffice/modules/master-data/academic-level/academicLevel.type.ts
@@ -7,6 +7,10 @@ export interface APIResponseAcademicLevel {
   updatedAt?: Date;
 }
 
+export interface IComboAcademicLevel {
+  data: APIResponseAcademicLevel[];
+}
+
 export interface AcademicLevelResponse {
   data: {
     items: APIResponseAcademicLevel[];
@@ -21,7 +25,11 @@ export interface IAcademicLevelView {
   data: APIResponseAcademicLevel[];
   openPopoverIndex: number | null;
   setOpenPopoverIndex: React.Dispatch<React.SetStateAction<number | null>>;
-  handleActionButtonRow: (id: string, action: 'delete' | 'edit', rowData?: string) => void;
+  handleActionButtonRow: (
+    id: string,
+    action: 'delete' | 'edit',
+    rowData?: string,
+  ) => void;
   handleAddAcademicLevel: (name: string) => Promise<void>;
   Filter: string;
   setFilter: (value: string) => void;
@@ -30,5 +38,3 @@ export interface IAcademicLevelView {
   fetchData: () => Promise<void>;
   role: number;
 }
-
-
