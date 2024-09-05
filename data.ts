ac704e50eff48a391f4a71e7283a3a8f2b74867d
arya-bhanu <aryacreative.dev@gmail.com>
2024-09-05 10:53:18 +0700

    feat: auth role

---
+++ data.ts
@@ -0,0 +1,21 @@
+export const accessRole = {
+  admin: [
+    {
+      id: '1',
+      code: 'manage-modul.edit',
+      description: '',
+    },
+    {
+      id: '2',
+      code: 'manage-modul.delete',
+      description: '',
+    },
+  ],
+  mentor: [
+    {
+      id: '1',
+      code: 'manage-modul.edit',
+      description: '',
+    },
+  ],
+};
--- src/app/backoffice/manage-modul/page.tsx
+++ src/app/backoffice/manage-modul/page.tsx
@@ -1,6 +1,27 @@
 'use client';
 import ManageModul from '@/backoffice/modules/manage-modul';
+import { useAuth } from '@/contexts/AuthContext';
+import { accessRole } from 'data';
+import { useEffect } from 'react';
 const ManageModulIndex = () => {
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+      const role = user.role;
+      window.localStorage.removeItem('access');
+      if (role === 1) {
+        window.localStorage.setItem('access', JSON.stringify(accessRole.admin));
+        return;
+      }
+      if (role === 3) {
+        window.localStorage.setItem(
+          'access',
+          JSON.stringify(accessRole.mentor),
+        );
+        return;
+      }
+    }
+  }, [JSON.stringify(user)]);
   return <ManageModul />;
 };
 
