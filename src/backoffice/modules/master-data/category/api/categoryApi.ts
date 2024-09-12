25ade60d68b86deb9803cec9d5c08a3dc008a229
Dika <annkrey6@gmail.com>
2024-09-12 16:00:46 +0700

    fix: api category with fetchAxios

--- src/backoffice/modules/master-data/category/api/categoryApi.ts
+++ src/backoffice/modules/master-data/category/api/categoryApi.ts
@@ -1,58 +1,41 @@
-import axios from 'axios';
-
-const API_URL = 'https://api-talentnesia.skwn.dev/api/v1';
+import { fetchAxios } from '@/lib/fetchAxios';
+import { APIResponseCategory } from '../category.type';
 
 export const categoryAPI = {
   fetch: async () => {
-    try {
-      const response = await axios.get(`${API_URL}/category`);
-      return response.data.data.items;
-    } catch (error) {
-      console.error('Failed to fetch category', error);
-      return [];
-    }
+    return fetchAxios<{ data: { items: APIResponseCategory[] } }>({
+      url: `/v1/category`,
+      method: 'GET',
+    }).then((response) => response.data.items);
   },
 
   getById: async (id: string) => {
-    try {
-      const response = await axios.get(`${API_URL}/category/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error('Failed to fetch category details', error);
-      return;
-    }
+    return fetchAxios<{ data: APIResponseCategory }>({
+      url: `/v1/category/${id}`,
+      method: 'GET',
+    }).then((response) => response.data);
   },
 
   add: async (data: { name: string; code: string; status?: number }) => {
-    try {
-      const response = await axios.post(`${API_URL}/category`, data);
-      return response.data;
-    } catch (error) {
-      console.error('Failed to add category', error);
-      throw error; 
-    }
+    return fetchAxios<{ data: APIResponseCategory }>({
+      url: `/v1/category`,
+      method: 'POST',
+      formData: data,
+    }).then((response) => response.data);
   },
-  
-  
 
   update: async (id: string, data: { name: string; code: string; status?: number }) => {
-    try {
-      const response = await axios.put(`${API_URL}/category/${id}`, data);
-      return response.data;
-    } catch (error) {
-      console.error('Failed to update category', error);
-      return;
-    }
+    return fetchAxios<{ data: APIResponseCategory }>({
+      url: `/v1/category/${id}`,
+      method: 'PUT',
+      formData: data,
+    }).then((response) => response.data);
   },
 
   delete: async (id: string) => {
-    try {
-      const response = await axios.delete(`${API_URL}/category/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error('Failed to delete category', error);
-      return;
-    }
+    return fetchAxios<{ success: boolean }>({
+      url: `/v1/category/${id}`,
+      method: 'DELETE',
+    }).then((response) => response.success);
   }
 };
-
