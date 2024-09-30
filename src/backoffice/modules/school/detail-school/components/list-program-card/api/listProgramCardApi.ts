ba7461601e4cd15ac37f78460d854547e1e14e77
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-09-30 13:48:08 +0700

    enhance, fix bug page school

--- src/backoffice/modules/school/detail-school/components/list-program-card/api/listProgramCardApi.ts
+++ src/backoffice/modules/school/detail-school/components/list-program-card/api/listProgramCardApi.ts
@@ -1,13 +1,14 @@
-import axios from 'axios';
+import { fetchAxios } from '@/lib/fetchAxios';
 import { ListProgramCardType } from '../listProgramCard.type';
 
 export const ListProgramCardAPI = {
   fetchProgram: async (schoolId: string): Promise<ListProgramCardType[]> => {
     try {
-      const response = await axios.get(
-        `${process.env.API_SERVER_URL}/v1/manage-program/program/${schoolId}`,
-      );
-      const transformedData = response.data.data.items.map((item: ListProgramCardType) => ({
+      const response = await fetchAxios<{ data: { items: ListProgramCardType[] } }>({
+        url: `/v1/manage-program/program/${schoolId}`,
+        method: 'GET',
+      });
+      const transformedData = response.data.items.map((item: ListProgramCardType) => ({
         id: item.id,
         name: item.name,
         startDate: item.startDate,
@@ -25,8 +26,11 @@ export const ListProgramCardAPI = {
   
   fetchDetailProgram: async (id: string) => {
     try {
-      const response = await axios.get(`${process.env.API_SERVER_URL}/v1/manage-program/course/${id}`);
-      return response.data;
+      const response = await fetchAxios({
+        url: `/v1/manage-program/course/${id}`,
+        method: 'GET',
+      });
+      return response;
     } catch (error) {
       console.error('Error fetching student table data:', error);
       throw error;
--- src/backoffice/modules/school/detail-school/components/list-table-student/api/listTableStudentApi.ts
+++ src/backoffice/modules/school/detail-school/components/list-table-student/api/listTableStudentApi.ts
@@ -1,13 +1,27 @@
 import { backOfficeAPI } from '@/lib/axiosConfig';
+import { fetchAxios } from '@/lib/fetchAxios';
 
 export const ListTableStudentAPI = {
+  // fetch: async (id: string) => {
+  //   try {
+  //     const response = await backOfficeAPI.get(`/v1/user-educational-institution/table/${id}`);
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error('Error fetching student table data:', error);
+  //     throw error;
+  //   }
+  // },
+
   fetch: async (id: string) => {
     try {
-      const response = await backOfficeAPI.get(`/v1/user-educational-institution/table/${id}`);
+      const response = await fetchAxios({
+        url: `/v1/user-educational-institution/table/${id}`,
+        method: 'GET',
+      })
       return response.data;
     } catch (error) {
       console.error('Error fetching student table data:', error);
       throw error;
     }
-  },
+  }
 };
