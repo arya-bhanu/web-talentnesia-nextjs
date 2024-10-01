df9c7e0e47feb54e075a47658f726d591f6ed7de
Dika <annkrey6@gmail.com>
2024-10-01 15:32:53 +0700

    fix: layout sidebar

--- src/backoffice/modules/faq/Faq.tsx
+++ src/backoffice/modules/faq/Faq.tsx
@@ -3,18 +3,13 @@ import React, { useState, useMemo } from 'react';
 import FaqView from './Faq.view';
 import { FAQItem } from './faq.type';
 import { createColumnHelper, ColumnDef } from '@tanstack/react-table';
-import ModalForm from './components/modal-form-faq';
+import ModalForm from './components/modal-faq';
 
 const columnHelper = createColumnHelper<FAQItem>();
 
 const Faq: React.FC = () => {
   const [faqs, setFaqs] = useState<FAQItem[]>([
     { id: '1', question: 'Apa itu LMS Talentnesia?', answer: 'LMS Talentnesia adalah Learning Management System yang dirancang untuk mendukung proses pembelajaran dan pengembangan keterampilan di berbagai industri.' },
-    { id: '2', question: 'Apa saja fitur utama dari LMS Talentnesia?', answer: 'Anda akan menerima email dengan nomor pelacakan setelah pesanan Anda dikirim.' },
-    { id: '3', question: 'Siapa yang dapat menggunakan LMS Talentnesia?', answer: 'Ya, silakan hubungi tim penjualan kami untuk harga grosir.' },
-    { id: '4', question: 'Apakah LMS Talentnesia mendukung pembelajaran jarak jauh?', answer: 'Ya, silakan hubungi tim penjualan kami untuk harga grosir.' },
-    { id: '5', question: 'Apakah saya bisa membeli barang dalam jumlah besar?', answer: 'Ya, silakan hubungi tim penjualan kami untuk harga grosir.' },
-    { id: '6', question: 'Apakah saya bisa membeli barang dalam jumlah besar?', answer: 'Ya, silakan hubungi tim penjualan kami untuk harga grosir.' },
   ]);
 
   const [filter, setFilter] = useState('');
@@ -47,10 +42,8 @@ const Faq: React.FC = () => {
 
   const handleSaveFaq = async (id: string | undefined, data: any) => {
     if (id) {
-      // Edit existing FAQ
       setFaqs(faqs.map(faq => faq.id === id ? { ...faq, ...data } : faq));
     } else {
-      // Add new FAQ
       const newFaq: FAQItem = {
         id: String(faqs.length + 1),
         ...data
