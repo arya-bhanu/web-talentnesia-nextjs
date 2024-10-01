e1da001dbb8b82a6e02629766a9cdb4e4fa144c5
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-10-01 09:23:46 +0700

    add validation file type at add content manage modul

--- src/backoffice/modules/manage-modul/components/form-content/FormContent.tsx
+++ src/backoffice/modules/manage-modul/components/form-content/FormContent.tsx
@@ -4,24 +4,16 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchContent } from '../../api/manageModelApi';
 import { getImageUrl } from '@/backoffice/modules/school/api/minioApi';
 import Loading from '@/components/loading';
-import { z } from 'zod';
-import { useStatusModalStore } from '@/lib/store';
 
 const FormContent: React.FC<{ contentId?: string }> = ({ contentId }) => {
   const [time, setTime] = useState(new Date(0, 0, 0, 1, 0));
   const [fileUrl, setFileUrl] = useState<string>('');
   const [fileName, setFileName] = useState<string>('');
   const [fileType, setFileType] = useState<string>('1');
-  const { openModal } = useStatusModalStore();
 
   const { data: dataContent, isLoading } = useQuery({
     queryKey: ['chapter', contentId],
     queryFn: () => fetchContent(contentId),
-  });
-
-  const fileSchema = z.object({
-    file: z.instanceof(File),
-    type: z.string(),
   });
 
   useEffect(() => {
@@ -32,10 +24,10 @@ const FormContent: React.FC<{ contentId?: string }> = ({ contentId }) => {
       if (dataContent.data.body && dataContent.data.fileOrigin) {
         // getImageUrl(dataContent.data.body)
         //   .then((url) => {
-            setFileName(dataContent.data.fileOrigin);
-            setFileUrl(dataContent.data.body);
-          // })
-          // .catch(console.error);
+        setFileName(dataContent.data.fileOrigin);
+        setFileUrl(dataContent.data.body);
+        // })
+        // .catch(console.error);
       }
     }
   }, [dataContent]);
@@ -59,26 +51,9 @@ const FormContent: React.FC<{ contentId?: string }> = ({ contentId }) => {
     newFileName: string,
     newFileType: number,
   ) => {
-    try {
-      const file = new File([], newFileName);
-      const validationResult = fileSchema.parse({
-        file,
-        type: newFileType.toString(),
-      });
-
-      if (!validateFileType(validationResult.file, validationResult.type)) {
-        throw new Error('Invalid file type for the selected content type');
-      }
-
-      setFileUrl(newFileUrl);
-      setFileName(newFileName);
-      setFileType(newFileType.toString());
-    } catch (error) {
-      openModal({
-        status: 'error',
-        message: `Invalid file: ${error instanceof Error ? error.message : 'An unexpected error occurred'}`,
-      });
-    }
+    setFileUrl(newFileUrl);
+    setFileName(newFileName);
+    setFileType(newFileType.toString());
   };
 
   return (
