0629b7fb7689b6359c9855e364099e9586ac7b53
Fadhli <fadhli.sekawanmedia@gmail.com>
2024-10-02 10:50:42 +0700

    fix componetn date picker

--- src/backoffice/modules/cms/components/datepicker/Datepicker.tsx
+++ src/backoffice/modules/cms/components/datepicker/Datepicker.tsx
@@ -10,27 +10,32 @@ interface DatepickerProps {
   placeholder?: string;
 }
 
-export const Component: React.FC<DatepickerProps> = ({ id, onChange, value, placeholder = 'Select date' }) => {
+export const Component: React.FC<DatepickerProps> = ({
+  id,
+  onChange,
+  value,
+  placeholder = 'Select date',
+}) => {
   const handleChange = (date: Date) => {
     const nextDay = addDays(date, 1);
     onChange(nextDay);
   };
 
   const CalendarIcon = () => (
-     <Image
-       src="/img/manage-user/calendar.svg"
-       width={25}
-       height={24}
-       alt="Calendar"
-     />
-    );
+    <Image
+      src="/img/manage-user/calendar.svg"
+      width={25}
+      height={24}
+      alt="Calendar"
+    />
+  );
 
   return (
     <Datepicker
       id={id}
       icon={CalendarIcon}
       onSelectedDateChanged={handleChange}
-      value={value ? value.toISOString().split('T')[0] : ''}
+      value={value || undefined}
       placeholder={placeholder}
     />
   );
