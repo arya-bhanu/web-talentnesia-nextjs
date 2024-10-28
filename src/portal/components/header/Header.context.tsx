0acca0bcf3753c271e056d3a79a10c5e324818e3
arya-bhanu <aryacreative.dev@gmail.com>
2024-10-28 18:45:18 +0700

    remove header

--- src/portal/components/header/Header.context.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-import { HeaderObserver } from './header.type';
-
-export const ObserverContext = createContext<{
-  headerObserver: HeaderObserver;
-}>({
-  headerObserver: { inView: true }, // default value
-});
